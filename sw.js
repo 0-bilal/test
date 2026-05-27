b4dd1540a179f2a11b35088dc5e4c5ef246a64b1
0-bilal <140864157+0-bilal@users.noreply.github.com>
2026-05-27 11:12:01 +0300

    إعادة هيكلة المشروع بالكامل

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // هذا الكود يسمح للتطبيق بالعمل حتى في حالة ضعف الإنترنت
-  e.respondWith(fetch(e.request));
-});
